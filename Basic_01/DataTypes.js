aa2d90213b1ec48089e8e0578886ad7d22c56fc1
hujaifa18 <abidhasanhujaifa1215@gmail.com>
2025-12-15 23:54:50 +0600

    Day 01

---
+++ Basic_01/DataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs, not browser
+
+let name = "Hujaifa"
+let age = 21
+let isLoggedIn = false
+let state = null
+
+//number -> 2 to the power 53
+//bigint 
+//string -> ""
+//boolean -> true/flase
+//null -> standalone value
+//undefined 
+//symbol -> unique
+
+//object
+
+console.log(typeof null);
+console.log(typeof undefined);
