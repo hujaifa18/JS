1ae501d0905d60b0a93fdbf0f213d13f425b20f5
hujaifa18 <abidhasanhujaifa1215@gmail.com>
2025-12-17 17:31:23 +0600

    Day 03

--- Basic_01/ConversionOperation.js
+++ Basic_01/ConversionOperation.js
@@ -1,5 +1,5 @@
 let score = "33abc" // check with 33 , true , "Hujaifa", undefined
-console.log("Test 1 : ");
+console.log("           Test 1 : ");
 console.log(typeof score);
 let value = Number(score)
 console.log(typeof value);
@@ -12,7 +12,7 @@ console.log(value);
 
 let isLoggedIn = 1
 let inBoolean = Boolean(isLoggedIn)
-console.log("Test 2 : ");
+console.log("           Test 2 : ");
 console.log(inBoolean);
 // 1 -> true; 0 -> false
 // "" -> false
@@ -22,6 +22,47 @@ console.log(inBoolean);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log("Test 3 : ");
+console.log("           Test 3 : ");
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+console.log("           Test 4 : ");
+let x = 33
+let y = -x
+console.log(y);
+
+console.log("           Test 5 : ");
+console.log(3+3);
+console.log(4-3);
+console.log(4*3);
+console.log(4**3);
+console.log(4/2);
+
+
+console.log("           Test 6 : ");
+let str1 = "Abid "
+let str2 = "Hujaifa"
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log("           Test 7 :");
+console.log("1" + 2 );
+console.log(1 + "2" );
+console.log("1" + 2 + 2 );
+console.log(1 + 2 + "2" );
+
+console.log("           Test 8 :");
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+console.log("           Test 9 :");
+let counter = 100
+counter++
+console.log(counter);
+let counter2 = 100
+++counter2
+console.log(counter2);
+
